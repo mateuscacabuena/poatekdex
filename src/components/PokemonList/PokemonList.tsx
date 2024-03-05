86c51cd3e53592aea49756bac88b2900f4c3df73
Mateus <mateus.campos@poatek.com>
2024-03-05 17:12:40 -0300

    add: applying context

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -1,42 +1,57 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import "./styles.css";
 import { useNavigate } from "react-router-dom";
 import { Spinner } from "@chakra-ui/react";
-
-/*
-  fazer responsividade
-  fazer useContext
-  limpar comentários no fim deste código
-*/
+import { usePokemonContext } from "../../hooks/usePokemonContext";
+import { Pokemon } from "../../interface/interfaces";
 
 function PokemonList() {
   const navigate = useNavigate();
-  const [pokemonList, setPokemonList] = useState<any[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const { setPokemon, isLoading, pokemonList } = usePokemonContext();
 
-  function handlePokemonClick(pokemon: any) {
+  function handlePokemonClick(pokemon: Pokemon) {
+    const obj = {
+      abilities: pokemon.abilities,
+      description: "descricao",
+      height: pokemon.height / 10,
+      id: pokemon.id,
+      name: pokemon.name,
+      stats: pokemon.stats,
+      types: pokemon.types,
+      weight: pokemon.weight / 10,
+    };
+    setPokemon(obj);
     navigate("/" + pokemon.name);
   }
 
-  const fetchPokemons = async () => {
-    const promises = [];
-    for (let i = 1; i <= 151; i++) {
-      promises.push(
-        fetch(`https://pokeapi.co/api/v2/pokemon/${i}`).then((response) =>
-          response.json()
-        )
-      );
-    }
-
-    const pokemons = await Promise.all(promises);
-    setPokemonList(pokemons);
-    setIsLoading(false);
-  };
+  function showPokemons() {
+    return (
+      <>
+        {pokemonList.map((pokemon) => (
+          <div
+            className="pokemon-card"
+            key={pokemon.id}
+            onClick={() => handlePokemonClick(pokemon)}
+          >
+            <div className="number">
+              <p>#{pokemon.id}</p>
+            </div>
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
+              alt={pokemon.name}
+            />
+            <div className="name">
+              <p>{pokemon.name}</p>
+            </div>
+          </div>
+        ))}
+      </>
+    );
+  }
 
   useEffect(() => {
-    fetchPokemons();
-    //console.log(pokemonList);
-  }, []);
+    showPokemons();
+  }, [pokemonList]);
 
   return (
     <div className="pokemon-list">
@@ -45,43 +60,9 @@ function PokemonList() {
           <Spinner color="red" size={"xl"} />
         </div>
       )}
-      {pokemonList.map((pokemon) => (
-        <div
-          className="pokemon-card"
-          key={pokemon.id}
-          onClick={() => handlePokemonClick(pokemon)}
-        >
-          <div className="number">
-            <p>#{pokemon.id}</p>
-          </div>
-          <img
-            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
-            alt={pokemon.name}
-          />
-          <div className="name">
-            <p>{pokemon.name}</p>
-          </div>
-        </div>
-      ))}
+      {showPokemons()}
     </div>
   );
 }
 
 export default PokemonList;
-
-// console.log(pokemon.name, pokemon.id, pokemon.height, pokemon.weight, pokemon.types, pokemon.stats)
-
-{
-  /* <div className="pokemon-card" onClick={handlePokemonClick}>
-        <div className="number">
-          <p>#001</p>
-        </div>
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-          alt="bulbasaur"
-        />
-        <div className="name">
-          <p>bulbasaur</p>
-        </div>
-      </div> */
-}
--- src/components/PokemonScreen/PokemonScreen.tsx
+++ src/components/PokemonScreen/PokemonScreen.tsx
@@ -5,55 +5,67 @@ import pokeball from "../../assets/pokeball.svg";
 import About from "./About/About";
 import Types from "./Types/Types";
 import BaseStats from "./BaseStats/BaseStats";
-import { ArrowBackIcon, ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
+import {
+  ArrowBackIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+} from "@chakra-ui/icons";
+import { usePokemonContext } from "../../hooks/usePokemonContext";
 
 function PokemonScreen() {
   const navigate = useNavigate();
+  const { pokemon } = usePokemonContext();
+  const firstType = pokemon.types[0].type.name;
 
   function handleBack() {
     navigate("/");
   }
 
   return (
-    <div className="pokemon-screen">
-      <div className="title">
-        <IconButton
-          aria-label="Back"
-          icon={<ArrowBackIcon boxSize={"2rem"} color={"#FFFFFF"} />}
-          backgroundColor={"transparent"}
-          onClick={handleBack}
-        />
-        <h1 className="name">Pokémon Name</h1>
-        <p className="number">#999</p>
-      </div>
-      <img src={pokeball} alt="pokeball" className="pokeball-icon" />
-      <div className="image-container">
-        <IconButton
-          aria-label="Back"
-          icon={<ChevronLeftIcon boxSize={"1.5rem"} color={"#FFFFFF"} />}
-          backgroundColor={"transparent"}
-        />
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-          alt="bulbasaur"
-          className="pokemon-image"
-        />
-        <IconButton
-          aria-label="Back"
-          icon={<ChevronRightIcon boxSize={"1.5rem"} color={"#FFFFFF"} />}
-          backgroundColor={"transparent"}
-        />
-      </div>
-      <div className="info">
-        <Types />
-        <h2>About</h2>
-        <About />
-        <p className="description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc iaculis
-          eros vitae tellus condimentum maximus sit amet in eros.
-        </p>
-        <h2>Base Stats</h2>
-        <BaseStats />
+    <div className="background">
+      <div className={'pokemon-screen ' + firstType}>
+        <div className="title">
+          <IconButton
+            aria-label="Back"
+            icon={<ArrowBackIcon boxSize={"2rem"} color={"#FFFFFF"} />}
+            backgroundColor={"transparent"}
+            _hover={{ backgroundColor: "transparent", opacity: 0.5}}
+            onClick={handleBack}
+          />
+          <h1 className="name">{pokemon.name}</h1>
+          <p className="number">#{pokemon.id}</p>
+        </div>
+        <img src={pokeball} alt="pokeball" className="pokeball-icon" />
+        <div className="image-container">
+          <IconButton
+            aria-label="Back"
+            icon={<ChevronLeftIcon boxSize={"1.5rem"} color={"#FFFFFF"} />}
+            backgroundColor={"transparent"}
+            _hover={{ backgroundColor: "transparent", opacity: 0.5}}
+          />
+          <img
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
+            alt="pokemon image"
+            className="pokemon-image"
+          />
+          <IconButton
+            aria-label="Back"
+            icon={<ChevronRightIcon boxSize={"1.5rem"} color={"#FFFFFF"} />}
+            backgroundColor={"transparent"}
+            _hover={{ backgroundColor: "transparent", opacity: 0.5}}
+          />
+        </div>
+        <div className="info">
+          <Types />
+          <h2>About</h2>
+          <About />
+          <p className="description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+            iaculis eros vitae tellus condimentum maximus sit amet in eros.
+          </p>
+          <h2>Base Stats</h2>
+          <BaseStats />
+        </div>
       </div>
     </div>
   );
