2efc5c55abc099ec95b8123a1c6131f267f4ba4d
Mateus <mateus.campos@poatek.com>
2024-03-12 13:01:48 -0300

    fix: pokemon screen modal place

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,23 @@ import "./App.css";
 import SearchBar from "./components/SearchBar/SearchBar";
 import Title from "./components/Title/Title";
 import PokemonList from "./components/PokemonList/PokemonList";
+import { usePokemonContext } from "./hooks/usePokemonContext";
+import { useDisclosure } from "@chakra-ui/react";
+import PokemonScreen from "./components/PokemonScreen/PokemonScreen";
 
 function App() {
+  const { pokemon } = usePokemonContext();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <div className="App">
       <header>
         <Title />
-        <SearchBar />
+        <SearchBar onOpen={onOpen} />
       </header>
       <main>
-        <PokemonList />
+        <PokemonList onOpen={onOpen} />
+        {pokemon && <PokemonScreen isOpen={isOpen} onClose={onClose} />}
       </main>
     </div>
   );
--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -1,23 +1,25 @@
 import "./styles.css";
-import { Spinner, useDisclosure } from "@chakra-ui/react";
+import { Spinner } from "@chakra-ui/react";
 import { usePokemonContext } from "../../hooks/usePokemonContext";
 import { Pokemon } from "../../interface/interfaces";
-import PokemonScreen from "../PokemonScreen/PokemonScreen";
 
-function PokemonList() {
-  const { pokemon, setPokemon, isLoading, pokemonList } = usePokemonContext();
-  const { isOpen, onOpen, onClose } = useDisclosure();
+interface Props {
+  onOpen: () => void;
+}
+
+function PokemonList( { onOpen }: Props) {
+  const { setPokemon, isLoading, pokemonList } = usePokemonContext();
 
   function handlePokemonClick(pokemon: Pokemon) {
     const obj = {
       abilities: pokemon.abilities,
       description: "descricao",
-      height: pokemon.height / 10,
+      height: pokemon.height,
       id: pokemon.id,
       name: pokemon.name,
       stats: pokemon.stats,
       types: pokemon.types,
-      weight: pokemon.weight / 10,
+      weight: pokemon.weight,
     };
     setPokemon(obj);
     onOpen();
@@ -50,7 +52,6 @@ function PokemonList() {
           </div>
         ))}
       </div>
-      {pokemon && <PokemonScreen isOpen={isOpen} onClose={onClose} />}
     </>
   );
 }
