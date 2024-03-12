7f8858d097e06adacb554e22531d837ddf578e4c
Mateus <mateus.campos@poatek.com>
2024-03-12 14:14:01 -0300

    add: next/previous pokemon functionality

--- src/components/PokemonScreen/PokemonScreen.tsx
+++ src/components/PokemonScreen/PokemonScreen.tsx
@@ -15,6 +15,7 @@ import {
   ChevronRightIcon,
 } from "@chakra-ui/icons";
 import { usePokemonContext } from "../../hooks/usePokemonContext";
+import { useEffect, useState } from "react";
 
 interface Props {
   isOpen: boolean;
@@ -22,17 +23,57 @@ interface Props {
 }
 
 function PokemonScreen({ isOpen, onClose }: Props) {
-  const { pokemon } = usePokemonContext();
-  const firstType = pokemon.types[0].type.name;
+  const { pokemon, setPokemon, pokemonList } = usePokemonContext();
+  const [slideAnimation, setSlideAnimation] = useState("");
+  const firstType = pokemon.types[0];
+
+  function handleNextPokemon() {
+    const nextPokemon = pokemonList.find((p) => p.id === pokemon.id + 1);
+    if (nextPokemon) {
+      setSlideAnimation("slide-left");
+      setTimeout(() => {
+        setPokemon(nextPokemon);
+        setSlideAnimation("");
+      }, 100); 
+    }
+  }
+
+  function handlePreviousPokemon() {
+    const previousPokemon = pokemonList.find((p) => p.id === pokemon.id - 1);
+    if (previousPokemon) {
+      setSlideAnimation("slide-right");
+      setTimeout(() => {
+        setPokemon(previousPokemon);
+        setSlideAnimation("");
+      }, 100); 
+    }
+  }
+
+  useEffect(() => {
+    const handleKeyDown = (event: any) => {
+      if (event.key === "ArrowRight") {
+        handleNextPokemon();
+      } else if (event.key === "ArrowLeft") {
+        handlePreviousPokemon();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleNextPokemon, handlePreviousPokemon]);
 
   return (
       <Modal isOpen={isOpen} onClose={onClose} isCentered>
-        <ModalOverlay backdropFilter='blur(5px)'/>
-        <ModalContent>
-          <div className={"pokemon-screen " + firstType}>
+        <ModalOverlay backdropFilter="blur(5px)" />
+        <ModalContent bg={'transparent'}>
+          <div className={"pokemon-screen " + firstType + " " + slideAnimation}>
             <div className="title">
               <IconButton
-                aria-label="Back"
+                aria-label="Close"
+                isRound
                 icon={<ArrowBackIcon boxSize={"2rem"} color={"#FFFFFF"} />}
                 backgroundColor={"transparent"}
                 _hover={{ backgroundColor: "transparent", opacity: 0.5 }}
@@ -45,9 +86,11 @@ function PokemonScreen({ isOpen, onClose }: Props) {
             <div className="image-container">
               <IconButton
                 aria-label="Back"
+                isRound
                 icon={<ChevronLeftIcon boxSize={"1.5rem"} color={"#FFFFFF"} />}
                 backgroundColor={"transparent"}
                 _hover={{ backgroundColor: "transparent", opacity: 0.5 }}
+                onClick={handlePreviousPokemon}
               />
               <img
                 src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
@@ -56,9 +99,11 @@ function PokemonScreen({ isOpen, onClose }: Props) {
               />
               <IconButton
                 aria-label="Back"
+                isRound
                 icon={<ChevronRightIcon boxSize={"1.5rem"} color={"#FFFFFF"} />}
                 backgroundColor={"transparent"}
                 _hover={{ backgroundColor: "transparent", opacity: 0.5 }}
+                onClick={handleNextPokemon}
               />
             </div>
             <div className="info">
