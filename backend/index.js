b9649a3e7229f6587c4f26b621516ae3538a1e02
Mateus <mateus.campos@poatek.com>
2024-04-23 16:14:27 -0300

    add: backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
-console.log('Hello World!')
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+//Config JSON response
+app.use(express.json());
+
+//Config CORS
+app.use(cors({ credentials: true,  origin: 'https://localhost:3000'}));
+
+app.listen(5000)
