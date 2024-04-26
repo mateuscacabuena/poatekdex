59b731ecec46bbb630b7d37ced68f8d82d136b44
Mateus <mateus.campos@poatek.com>
2024-04-26 15:12:42 -0300

    add: pokemon dtos

---
+++ backend/src/pokemon/dto/create-pokemon.dto.ts
@@ -0,0 +1,18 @@
+export class CreatePokemonDto {
+  id: number;
+  name: string;
+  type: [string];
+  baseExperience: number;
+  height: number;
+  weight: number;
+  abilities: [string];
+  stats: {
+    hp: number;
+    attack: number;
+    defense: number;
+    specialAttack: number;
+    specialDefense: number;
+    speed: number;
+  };
+  imageUrl: string;
+}
---
+++ backend/src/pokemon/dto/update-pokemon.dto.ts
@@ -0,0 +1,18 @@
+export class UpdatePokemonDto {
+    id: number;
+    name: string;
+    type: [string];
+    baseExperience: number;
+    height: number;
+    weight: number;
+    abilities: [string];
+    stats: {
+      hp: number;
+      attack: number;
+      defense: number;
+      specialAttack: number;
+      specialDefense: number;
+      speed: number;
+    };
+    imageUrl: string;
+  }
