74222a63cf2fc1fb41626f172692dd58a940ce54
Mateus <mateus.campos@poatek.com>
2024-05-03 13:15:12 -0300

    fix: cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: '*',
+  };
+
+  app.enableCors(corsOptions);
   app.setGlobalPrefix('api');
 
   if (module.hot) {
